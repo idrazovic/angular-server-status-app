00fc8c673fe99a05f35822c8b220ea9729bdb2d3
idrazovic <idrazovic@gmail.com>
2024-07-24 12:02:42 +0200

    project structure updated

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,25 @@
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-control',
+    standalone: true,
+    imports: [],
+    templateUrl: './control.component.html',
+    styleUrl: './control.component.css',
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        class: 'control',
+        '(click)': 'onClick()'
+    }
+})
+export class ControlComponent {
+    // @HostBinding('class') className = 'control';
+    // @HostListener('click') onClick() {
+    //     console.log('clicked');
+    // }
+    label = input.required<string>();
+
+    onClick() { 
+        console.log('clicked');
+    }
+}
